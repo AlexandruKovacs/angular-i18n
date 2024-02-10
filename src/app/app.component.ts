f31d5019b04eea524a433203e8542935fed44e36
Alex Kovacs <axkovacs27@gmail.com>
2024-02-10 18:08:00 +0100

    demo-app-i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-i18n';
+
+  constructor(private translate: TranslateService) {}
+
+  ngOnInit(): void {
+    this.initLanguage();
+  }
+
+  initLanguage(): void {
+
+    const lang = sessionStorage.getItem('lang');
+
+    if (lang) {
+      this.translate.use(lang);
+    } else {
+      this.translate.setDefaultLang('en');
+      this.translate.use('en');
+    }
+  }
+
+  useLanguage(language: string): void {
+    this.translate.use(language);
+    sessionStorage.setItem('SAVED_LANGUAGE', language);
+  }
 }
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+        }
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
+
+// Requerido para compliación AOT (ahead of time)
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
