e06ef95eb51fd74bad68f4d41fe2f43b41ccc1f5
Alex Kovacs <axkovacs27@gmail.com>
2024-02-10 18:43:01 +0100

    i18n desde providers app.config.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { TranslateService } from "@ngx-translate/core";
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, TranslateModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
 
   constructor(private translate: TranslateService) {}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,29 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+
+
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(TranslateModule.forRoot({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+        }
+      }),
+    ),
+    provideRouter(routes)
+  ]
 };
+
+// Requerido para compliación AOT (ahead of time)
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
